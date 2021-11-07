9c4ee9d91a419cf5ddd1ebd05092e36f171b3270
shivamgupta-07 <shivamvk183@gmail.com>
2021-11-07 21:19:24 +0530

    Completed landing page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route exact path = "" element = {<Login />}></Route>
+          
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Home = (props) =>{
+    return(
+        <div>
+            This is home
+        </div>
+    )
+}
+
+export default Home
